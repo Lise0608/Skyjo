db1cdd3340a70318a31e718b65885a1478db82fc
lise0608 <lise.porcheron37@gmail.com>
2023-12-12 10:29:35 +0100

    ajout src

---
+++ src/main/java/skyjo/dao/JpaContext.java
@@ -0,0 +1,53 @@
+package eshop.dao;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaContext {
+	private static EntityManagerFactory emf = null;
+
+	private static DaoClient daoClient = new DaoClientImpl();
+	private static DaoFournisseur daoFournisseur = new DaoFournisseurImpl();
+	private static DaoCommande daoCommande = new DaoCommandeImpl();
+	private static DaoProduit daoProduit = new DaoProduitImpl();
+	private static DaoCategorie daoCategorie = new DaoCategorieImpl();
+	private static DaoLigneCommande daoLigneCommande = new DaoLigneCommandeJpaImpl();
+
+	public static DaoLigneCommande getDaoLigneCommande() {
+		return daoLigneCommande;
+	}
+
+	public static DaoClient getDaoClient() {
+		return daoClient;
+	}
+
+	public static DaoFournisseur getDaoFournisseur() {
+		return daoFournisseur;
+	}
+
+	public static DaoCommande getDaoCommande() {
+		return daoCommande;
+	}
+
+	public static DaoProduit getDaoProduit() {
+		return daoProduit;
+	}
+
+	public static DaoCategorie getDaoCategorie() {
+		return daoCategorie;
+	}
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("jpa");
+		}
+		return emf;
+	}
+
+	public static void closeJpa() {
+		if (emf != null) {
+			emf.close();
+			emf = null;
+		}
+	}
+}
