223793a1fc50a1f2e52e7bf4ff4663ada50e9521
lise0608 <lise.porcheron37@gmail.com>
2023-12-12 15:26:31 +0100

    ajout du DaoPlayer

---
+++ src/main/java/skyjo/dao/DaoPlayer.java
@@ -0,0 +1,9 @@
+package skyjo.dao;
+
+import java.util.List;
+
+import skyjo.entities.Player;
+
+public interface DaoPlayer extends DaoGeneric<Player, Long>{
+//	public List<Player> findTop10PlayersByScore();
+}
