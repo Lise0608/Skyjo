c0bb34a67379640fc2623eb9a26c3f08c6ea7b93
lise0608 <lise.porcheron37@gmail.com>
2023-12-12 15:47:52 +0100

    modification de Long en PlayedId

--- src/main/java/skyjo/dao/DaoPlayer.java
+++ src/main/java/skyjo/dao/DaoPlayer.java
@@ -3,7 +3,8 @@ package skyjo.dao;
 import java.util.List;
 
 import skyjo.entities.Player;
+import skyjo.entities.PlayerId;
 
-public interface DaoPlayer extends DaoGeneric<Player, Long>{
+public interface DaoPlayer extends DaoGeneric<Player, PlayerId>{
 //	public List<Player> findTop10PlayersByScore();
 }
