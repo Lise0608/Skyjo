c03d1fca550daf808a019ff3de31e7c2e9f7bb3b
lise0608 <lise.porcheron37@gmail.com>
2023-12-12 15:27:37 +0100

    ajout du DaoPlayerId

---
+++ src/main/java/skyjo/dao/DaoPlayerId.java
@@ -0,0 +1,13 @@
+package skyjo.dao;
+
+import java.util.List;
+
+import skyjo.entities.Game;
+import skyjo.entities.Player;
+import skyjo.entities.PlayerId;
+import skyjo.entities.User;
+
+public interface DaoPlayerId extends DaoGeneric<PlayerId, Long>{
+//	public PlayerId getPlayerIdByUserAndGame(User user, Game game);
+//	public List<PlayerId> getPlayerIdByUser(User user);
+}
