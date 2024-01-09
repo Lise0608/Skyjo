41ada181675a9afff9fbc1bcdbf4751ac547972f
Angeloo59 <casu.angelo0310@gmail.com>
2024-01-09 15:59:33 +0100

    modif WebConfig

--- skyjoRest/src/main/java/skyjoRest/config/WebConfig.java
+++ skyjoRest/src/main/java/skyjoRest/config/WebConfig.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import formationJpa.config.JpaConfig;
+import skyjo.config.JpaConfig;
+
 
 @Configuration
 @EnableWebMvc
