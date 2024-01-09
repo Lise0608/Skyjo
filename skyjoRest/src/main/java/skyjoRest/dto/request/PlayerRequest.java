92cad9c0300c411263aea2d81e1f36e1116c5cb7
Cesar-Hunault <cesarhunault@gmail.com>
2024-01-09 16:04:28 +0100

    première version playerrequest

--- skyjoRest/src/main/java/skyjoRest/dto/request/PlayerRequest.java
+++ skyjoRest/src/main/java/skyjoRest/dto/request/PlayerRequest.java
@@ -1,5 +1,18 @@
 package skyjoRest.dto.request;
 
-public class PlayerRequest {
+import jakarta.validation.constraints.Min;
 
+public class PlayerRequest {
+	
+	private Long userid;
+	private Long gameId;
+	
+	@Min(value = 0)
+	private int score;
+	
+	public PlayerRequest() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
