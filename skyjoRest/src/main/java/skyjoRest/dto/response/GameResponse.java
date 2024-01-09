aaa82f29e7dd9c1bb551ddde3ba4a33021b2af1a
guillaumeprevot1 <guillaume9809@gmail.com>
2024-01-09 16:18:03 +0100

    PlayerResponse et GameResponse

--- skyjoRest/src/main/java/skyjoRest/dto/response/GameResponse.java
+++ skyjoRest/src/main/java/skyjoRest/dto/response/GameResponse.java
@@ -1,5 +1,64 @@
 package skyjoRest.dto.response;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeanUtils;
+
 public class GameResponse {
 
+	private Long id; 
+	private GameMode gameMode;
+	private Deck deck;
+	private List <PlayerResponse> players;
+
+	public GameResponse () {
+
+	}
+
+	public GameResponse (Game gameEntity) {
+		this(gameEntity, false);
+	}
+
+	public GameResponse (Game gameEntity, boolean loadPlayers) {
+		BeanUtils.copyProperties (gameEntity, this, "players");
+		if (loadPlayers) {
+			if (gameEntity.getPlayers()!= null) {
+				gameEntity.getPlayers().stream().map(p-> new PlayerResponse(p)).collect(Collectors.toList());		
+			}
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public GameMode getGameMode() {
+		return gameMode;
+	}
+
+	public void setGameMode(GameMode gameMode) {
+		this.gameMode = gameMode;
+	}
+
+	public Deck getDeck() {
+		return deck;
+	}
+
+	public void setDeck(Deck deck) {
+		this.deck = deck;
+	}
+
+	public List<PlayerResponse> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<PlayerResponse> players) {
+		this.players = players;
+	}
+	
 }
--- skyjoRest/src/main/java/skyjoRest/dto/response/PlayerResponse.java
+++ skyjoRest/src/main/java/skyjoRest/dto/response/PlayerResponse.java
@@ -1,5 +1,33 @@
 package skyjoRest.dto.response;
 
+import org.springframework.beans.BeanUtils;
+
 public class PlayerResponse {
+	private PlayerId id; 
+	private int score;
+	
+	public PlayerResponse () {
+		
+	}
+	
+	public PlayerResponse (Player playerEntity) {
+		BeanUtils.copyProperties (playerEntity , this);
+	}
+
+	public PlayerId getId() {
+		return id;
+	}
+
+	public void setId(PlayerId id) {
+		this.id = id;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
 
 }
