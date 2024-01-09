37f4af3c2e77cdc0bc70d2ac9795eb3002ecf42c
Angeloo59 <casu.angelo0310@gmail.com>
2024-01-09 17:23:26 +0100

    ajout PlayerRepositories

--- skyjoJpa/src/main/java/skyjo/repositories/PlayerRepositories.java
+++ skyjoJpa/src/main/java/skyjo/repositories/PlayerRepositories.java
@@ -1,5 +1,10 @@
 package skyjo.repositories;
 
-public class PlayerRepositories {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import skyjo.entities.Player;
+import skyjo.entities.PlayerId;
+
+public interface PlayerRepositories extends JpaRepository<Player, PlayerId> {
 
 }
