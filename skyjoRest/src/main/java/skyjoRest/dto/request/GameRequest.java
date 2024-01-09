a3ff5fccd331ad40513cbe1b774bd6e54490a825
lise0608 <lise.porcheron37@gmail.com>
2024-01-09 15:35:16 +0100

    ajout fichier game et compte request

--- skyjoRest/src/main/java/skyjoRest/dto/request/GameRequest.java
+++ skyjoRest/src/main/java/skyjoRest/dto/request/GameRequest.java
@@ -1,5 +1,48 @@
 package skyjoRest.dto.request;
 
-public class GameRequest {
+import java.util.List;
 
+import skyjo.entities.Player;
+
+public class GameRequest {
+		private Long id; 
+		private GameMode gameMode;
+		private Deck deck;
+		private List <Player> players;
+		
+		public GameRequest() {
+
+		}
+
+		public synchronized Long getId() {
+			return id;
+		}
+
+		public synchronized void setId(Long id) {
+			this.id = id;
+		}
+
+		public synchronized GameMode getGameMode() {
+			return gameMode;
+		}
+
+		public synchronized void setGameMode(GameMode gameMode) {
+			this.gameMode = gameMode;
+		}
+
+		public synchronized Deck getDeck() {
+			return deck;
+		}
+
+		public synchronized void setDeck(Deck deck) {
+			this.deck = deck;
+		}
+
+		public synchronized Long getPlayerId() {
+			return playerId;
+		}
+
+		public synchronized void setPlayerId(Long playerId) {
+			this.playerId = playerId;
+		}
 }
