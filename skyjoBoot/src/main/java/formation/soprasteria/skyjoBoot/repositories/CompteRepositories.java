0b11ea18e23ef5e14162613bd8ef260d30b1bb37
lise0608 <lise.porcheron37@gmail.com>
2024-01-17 16:58:30 +0100

    ajout de CompteRepositories

--- skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/repositories/CompteRepositories.java
+++ skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/repositories/CompteRepositories.java
@@ -1,10 +1,16 @@
 package formation.soprasteria.skyjoBoot.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import formation.soprasteria.skyjoBoot.entities.Compte;
 
 
 public interface CompteRepositories extends JpaRepository<Compte, Long> {
+	List<Compte> findByLogin(String login);
 
+	Optional<Compte> findByLoginAndIdNot(String login, Long id);
 }
+
