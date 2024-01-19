22d1d690901b5240ca111b8131a811e00b2562fd
Cesar-Hunault <cesarhunault@gmail.com>
2024-01-19 10:11:27 +0100

    Ajout delete game dans GameRestController et savePlayer dans PlayerRestController, appelé lors de la sauvegarde d'une partie

--- skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/restController/GameRestController.java
+++ skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/restController/GameRestController.java
@@ -7,9 +7,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -72,6 +75,12 @@ public class GameRestController {
 		gameEntity.setPlayers(players);
 
 		return gameEntity;
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteGameById(@PathVariable Long id) {
+		gameSrv.deleteById(id);
 	}
 
 }
--- skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/restController/PlayerRestController.java
+++ skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/restController/PlayerRestController.java
@@ -6,10 +6,12 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import formation.soprasteria.skyjoBoot.dtoresponse.PlayerResponse;
+import formation.soprasteria.skyjoBoot.entities.Player;
 import formation.soprasteria.skyjoBoot.services.PlayerService;
 
 @RestController
@@ -36,7 +38,10 @@ public class PlayerRestController {
 	
 //	@DeleteMapping
 	
-//	@PostMapping
+	@PostMapping
+	public PlayerResponse savePlayer(Player player) {
+		return new PlayerResponse(playerService.create(player));
+	}
 	
 //	@PutMapping
 }
