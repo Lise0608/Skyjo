eda5fb4061e429fca6cfd83bff5b9db39dad6a48
Angeloo59 <casu.angelo0310@gmail.com>
2024-01-19 17:59:10 +0100

    modif Compte

--- skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/entities/Compte.java
+++ skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/entities/Compte.java
@@ -1,89 +1,87 @@
 package formation.soprasteria.skyjoBoot.entities;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Objects;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
-import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "compte")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "type",discriminatorType = DiscriminatorType.STRING,length = 1)
-public class Compte {
+public class Compte implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id_compte")
 	private Long id;
-	@Column(name="login_compte")
+	@Column(name = "login", nullable = false, unique = true)
 	private String login;
-	@Column(name="password_compte")
-	private String password;
-	
-	
+	@Column(name = "mdp", nullable = false)
+	private String mdp;
+	@Column(name = "role", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	@Column(name = "email", nullable = false)
+	private String email;
+
 	public Compte() {
-		super();
+
 	}
 
-
-	public Compte(String login, String password) {
-		super();
-		this.login = login;
-		this.password = password;
+	public String getEmail() {
+		return email;
 	}
 
-
-	public Compte(Long id, String login, String password) {
-		super();
-		this.id = id;
-		this.login = login;
-		this.password = password;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getLogin() {
 		return login;
 	}
 
-
 	public void setLogin(String login) {
 		this.login = login;
 	}
 
-
-	public String getPassword() {
-		return password;
+	public String getMdp() {
+		return mdp;
 	}
 
-
-	public void setPassword(String password) {
-		this.password = password;
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
 	}
 
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -96,6 +94,41 @@ public class Compte {
 		Compte other = (Compte) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// 1 role=>1 authority
+		return Arrays.asList(new SimpleGrantedAuthority(role.toString()));
+	}
+
+	@Override
+	public String getPassword() {
+		return mdp;
+	}
+
+	@Override
+	public String getUsername() {
+		return login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
 }
