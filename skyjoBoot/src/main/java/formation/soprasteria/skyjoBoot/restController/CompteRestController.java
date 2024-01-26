af56ce076fd2536d08af38e8460fe5aa18414418
Angeloo59 <casu.angelo0310@gmail.com>
2024-01-26 23:27:07 +0100

    modif CompteRestController

--- skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/restController/CompteRestController.java
+++ skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/restController/CompteRestController.java
@@ -6,60 +6,87 @@ import java.util.stream.Collectors;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import formation.soprasteria.skyjoBoot.dtorequest.CompteRequest;
 import formation.soprasteria.skyjoBoot.dtoresponse.CompteResponse;
+import formation.soprasteria.skyjoBoot.dtoresponse.JsonViews;
 import formation.soprasteria.skyjoBoot.entities.Compte;
+import formation.soprasteria.skyjoBoot.entities.Role;
 import formation.soprasteria.skyjoBoot.services.CompteService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/compte")
-@CrossOrigin(origins = "*")
 public class CompteRestController {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
-	private CompteService compteService;
+	private CompteService compteSrv;
 	
 	@GetMapping ("")
 	public List<CompteResponse> listeDesComptes () {
-		return compteService.findAll().stream().map(c -> new CompteResponse(c)).collect(Collectors.toList());
+		return compteSrv.findAll().stream().map(c -> new CompteResponse(c)).collect(Collectors.toList());
 	}
 	
 	@GetMapping("/{id}")
 	public CompteResponse compteById (@PathVariable Long id) {
-		return new CompteResponse(compteService.findById(id));
+		return new CompteResponse(compteSrv.findById(id));
 	}
 	
 	@DeleteMapping ("/{id}")
 	@ResponseStatus (code = HttpStatus.NO_CONTENT)
 	public void deleteCompte (@PathVariable Long id) {
-		compteService.deleteById(id);
+		compteSrv.deleteById(id);
 	}
-	
-	
 	
 	@PutMapping ("/{id}")
 	public CompteResponse updateCompte (@PathVariable Long id, @Valid @RequestBody CompteRequest compteRequest, BindingResult br) {
 		if (br.hasErrors()) {
     		throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
     	}
-		Compte compteAModifier = compteService.findById (id);
+		Compte compteAModifier = compteSrv.findById (id);
 		if (compteAModifier == null) {
 			 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Compte inconnu");
 		}
 		BeanUtils.copyProperties (compteRequest, compteAModifier);
-		return new CompteResponse(compteService.update(compteAModifier));
+		return new CompteResponse(compteSrv.update(compteAModifier));
+	}
+	
+
+	@GetMapping("/api/auth")
+	@SecurityRequirement(name = "basicAuth")
+	@JsonView(JsonViews.Compte.class)
+	public CompteResponse auth(@AuthenticationPrincipal Compte compte) {
+		return new CompteResponse(compte);
+	}
+
+	@PostMapping("/api/inscription")
+	@JsonView(JsonViews.Compte.class)
+	public CompteResponse inscription(@Valid @RequestBody CompteRequest compteRequest, BindingResult br) {
+		if (br.hasErrors()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		Compte compte = new Compte();
+		compte.setLogin(compteRequest.getLogin());
+		compte.setPassword(passwordEncoder.encode(compteRequest.getPassword()));
+		compte.setEmail(compteRequest.getEmail());
+		compte.setRole(Role.ROLE_USER);
+		return new CompteResponse(compteSrv.create(compte));
 	}
 }
