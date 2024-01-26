49d52e6e55afcb17a65ceb538f4d53ce95c5f309
Cesar-Hunault <cesarhunault@gmail.com>
2024-01-26 14:17:15 +0100

    Ajout CrossOrigin dans controllers classiques et CompteRestController

--- skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/controller/CompteController.java
+++ skyjoBoot/src/main/java/formation/soprasteria/skyjoBoot/controller/CompteController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import formation.soprasteria.skyjoBoot.entities.Role;
 import formation.soprasteria.skyjoBoot.repositories.CompteRepositories;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class CompteController {
 
 	@Autowired
